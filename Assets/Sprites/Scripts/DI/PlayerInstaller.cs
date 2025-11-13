fee0cc92bb765370b621d5b1288059be88335ec7
Igor <149200506+kirishirorin@users.noreply.github.com>
2025-11-13 18:26:32 +0300

    Initial commit

---
+++ Assets/Sprites/Scripts/DI/PlayerInstaller.cs
@@ -0,0 +1,17 @@
+﻿using Player;
+using UnityEngine;
+using Zenject;
+
+namespace Sprites.Scripts.DI
+{
+    public class PlayerInstaller: MonoInstaller
+
+    {
+        [SerializeField] private PlayerMovement _playerMovement;
+        
+        override public void InstallBindings()
+        {
+            Container.Bind<PlayerMovement>().FromInstance(_playerMovement).AsSingle().NonLazy();
+        }
+    }
+}
---
+++ Assets/Sprites/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Player
+{
+    public class PlayerMovement : MonoBehaviour
+    {
+        [SerializeField] private float _moveSpeed;
+        [SerializeField] private Animator _animator;
+        private Vector3 _movement;
+        private Vector3 Movement => _movement;
+
+        void Update()
+        {
+            Move();
+        }
+
+        public void Move()
+        {
+            _movement = new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), 0);
+            //_movement = new Vector3(Joystick.Horizontal, Joystick.Vertical, 0);
+            
+            if (_movement.x > 0)
+            {
+                _animator.SetBool("Left", false);
+                _animator.SetBool("Right", true);
+            }
+            else
+            {
+                _animator.SetBool("Right", false);
+                _animator.SetBool("Left", true);
+            }
+
+            if (_movement.y > 0)
+            {
+                _animator.SetBool("Down", false);
+                _animator.SetBool("Up", true);
+            }
+            else
+            {
+                _animator.SetBool("Up", false);
+                _animator.SetBool("Down", true);
+            }
+
+            transform.position += _movement.normalized * (_moveSpeed * Time.deltaTime);
+            _animator.SetFloat("Horizontal", _movement.x);
+            _animator.SetFloat("Vertical", _movement.y);
+            _animator.SetFloat("Speed", _movement.sqrMagnitude);
+        }
+    }
+}
